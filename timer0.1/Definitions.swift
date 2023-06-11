4cd40d350b561e972b1ba824dd3fd4a16bb3e4f5
tatiana talavera <iced.tati@gmail.com>
2023-06-11 12:25:18 -0400

    first commit

---
+++ timer0.1/Definitions.swift
@@ -0,0 +1,15 @@
+//
+//  Definitions.swift
+//  timer0.1
+//
+//  Created by Tatiana Talavera on 07-06-23.
+//
+
+struct TimerTask {
+    var name: String
+    var seconds: Int
+    var icon: String?
+    var color: String
+    var remaining: Int
+}
+
